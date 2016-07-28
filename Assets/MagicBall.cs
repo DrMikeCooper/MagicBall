4725a46edcfaca98b2351816ca656843a392fc6b
Dr_Mike_Cooper <coriolisboy@gmail.com>
2016-07-28 10:04:54 +0930

    Initial Checkin

---
+++ Assets/MagicBall.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicBall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+        if (Input.GetKey(KeyCode.A))
+            pos.x -= 0.1f;
+        if (Input.GetKey(KeyCode.D))
+            pos.x += 0.1f;
+        transform.position = pos;
+        if (Input.GetKeyDown(KeyCode.Space))
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 500, 0));
+    }
+}
