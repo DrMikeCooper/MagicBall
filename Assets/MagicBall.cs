b5fbb6e3ce8ac6b502c56a1dfaab5ba693570369
Dr_Mike_Cooper <coriolisboy@gmail.com>
2016-08-10 11:04:08 +0930

    Removed all spare code

--- Assets/MagicBall.cs
+++ Assets/MagicBall.cs
@@ -7,56 +7,11 @@ public class MagicBall : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        bool UserA = true;
+       
 	}
-
-    
-    void AddNavigation()
-    {
-        doNav();
-    }
-
-    // Dr Mike's branch
-    void MakeEverythingShiny(float alpha)
-    {
-    }
-
-    void MakeObjectShiny(GameObject obj)
-    {
-    }
-
-
-    //Player 2 Code
 
 	// Update is called once per frame
 	void Update () {
-        Vector3 pos = transform.position;
-        if (Input.GetKey(KeyCode.A))
-            pos.x -= 0.1f;
-        if (Input.GetKey(KeyCode.D))
-            pos.x += 0.1f;
-        transform.position = pos;
-        if (Input.GetKeyDown(KeyCode.W))
-            GetComponent<Rigidbody>().AddForce(new Vector3(0, 500, 0));
-        bool UserB = true;
+
     }
-
-void FunctionB()
-{
-}
-
-
-    void ConflictA()
-    {
-    }
-
-void ConflictB()
-{
-}
-    void NormalCode()
-    {
-        for (int i = 0; i < 10; i++)
-            Debug.Log(i);
-    }
-
 }
